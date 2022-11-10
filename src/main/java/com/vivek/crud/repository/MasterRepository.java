06942f14a34989ae7990db72e3dcd9946cdddb33
Vivek Mishra <100134407+mishravivek09@users.noreply.github.com>
2022-11-10 14:24:01 +0530

    Update MasterRepository.java

--- src/main/java/com/vivek/crud/repository/MasterRepository.java
+++ src/main/java/com/vivek/crud/repository/MasterRepository.java
@@ -2,6 +2,9 @@ package com.vivek.crud.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.vivek.crud.model.Master;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface MasterRepository extends JpaRepository<Master, Long>{
