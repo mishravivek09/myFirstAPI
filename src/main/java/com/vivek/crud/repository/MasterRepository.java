3985986cdddc8b4808f053378a9176ebfa674d73
Vivek Mishra <100134407+mishravivek09@users.noreply.github.com>
2022-11-10 14:23:07 +0530

    Update MasterRepository.java

--- src/main/java/com/vivek/crud/repository/MasterRepository.java
+++ src/main/java/com/vivek/crud/repository/MasterRepository.java
@@ -5,5 +5,6 @@ import com.vivek.crud.model.Master;
 
 @Repository
 public interface MasterRepository extends JpaRepository<Master, Long>{
-
+  @Query("select m from Master m where lower(m.name) like lower(concat('%', :nameToFind,'%'))")
+	List<Master> findByNameLike(@Param("nameToFind") String name);
 }
